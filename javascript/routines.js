594c3f422b1735b2b1c5895b17a5f089b3999516
Blocky7 <61798065+Blocky7@users.noreply.github.com>
2022-08-30 14:45:11 +0600

    Add files via upload

--- javascript/routines.js
+++ javascript/routines.js
@@ -23,17 +23,28 @@ function saveRoutine(object)
         var dr = {};
     dr[object.name] = object.value;
     localStorage.dailyRoutines = JSON.stringify(dr);
+    if(object.value.length != 0)
+        document.location.reload();
 }
 
 if(localStorage.hasOwnProperty("dailyRoutines"))
     var dr = JSON.parse(localStorage.dailyRoutines);
 else
     var dr = {'monday': '', 'tuesday': '', 'wednesday': '', 'thursday': '', 'friday': '', 'saturday': '', 'sunday': ''};
+
+var reload = false;
 for(const [key, value] of Object.entries(dr)) {
     if(value in routines)
         document.getElementsByName(key)[0].value = value;
     else 
+    {
         saveRoutine(document.getElementsByName(key)[0]);
-    document.getElementById(key).addEventListener('click',function(){ window.location.href='./playRoutine.html?rot=' + value;});
-    document.getElementsByName(key)[0].addEventListener('click', function(e){ e.stopPropagation(); });
+    }
+    if(value.length != 0)
+    {
+        document.getElementById(key).addEventListener('click',function(){ window.location.href='./playRoutine.html?rot=' + value;});
+        document.getElementsByName(key)[0].addEventListener('click', function(e){ e.stopPropagation(); });
+    }
 }
+if(reload)
+    document.location.reload();
